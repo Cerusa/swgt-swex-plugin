e99de01de2e5bc39b7b13ec121dacbcf574d982e
David Grigsby <David.Grigsby@flexwareinnovation.com>
2021-03-10 17:44:47 -0500

    Update Siege History Aspect
    Added capability to log defense decks and their individual history

--- SWGTLogger/index.js
+++ SWGTLogger/index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const pluginName = 'SWGTLogger';
 var wizardBattles = [];
 var sendBattles = [];
+var tempDefenseDeckInfo = [];
+
+
 
 module.exports = {
   defaultConfig: {
@@ -37,7 +40,7 @@ module.exports = {
 
       //Guild Info
       'getGuildAttendInfo',
-      'GetGuildInfo',
+      //'GetGuildInfo',
 
       //Guild War
       'GetGuildWarBattleLogByGuildId',
@@ -71,6 +74,22 @@ module.exports = {
       'GetGuildSiegeMatchupInfo'
     ];
 	
+	var listenToSWGTHistoryCommands = [
+
+      //Siege Defense Units
+      'GetGuildSiegeBaseDefenseUnitList',
+      'GetGuildSiegeBaseDefenseUnitListPreset',
+	  'GetGuildSiegeDefenseDeckByWizardId',
+	  
+	  
+	  //Defense Log Link
+	  'GetGuildSiegeBattleLogByDeckId',
+	  
+	  //Battle Logs
+	  //'GetGuildSiegeBattleLog'
+    ];
+	
+	
 	proxy.log({ type: 'debug', source: 'plugin', name: this.pluginName, message: "Listening to commands: " + listenToSWGTCommands.toString().replace(/,/g,', ')+'<br><br>'+listenTo3MDCCommands.toString().replace(/,/g,', ') });
 
     //Attach SWGT events
@@ -89,6 +108,17 @@ module.exports = {
         });
       }
     }
+	//Attach SWGT Siege Log History Data
+	
+    if (config.Config.Plugins[pluginName].uploadBattles){
+      for (var commandIndex in listenToSWGTHistoryCommands) {
+        var command = listenToSWGTHistoryCommands[commandIndex];
+        proxy.on(command, (req, resp) => {
+          this.processSWGTHistoryRequest(command, proxy, config, req, resp, cache);
+        });
+      }
+    }
+	
   },
   hasAPISettings(config) {
     if (!config.Config.Plugins[pluginName].enabled) return false;
@@ -338,6 +368,120 @@ module.exports = {
       }
     }
   },
+  
+  processSWGTHistoryRequest(command, proxy, config, req, resp, cache) {
+	    if( resp['command'] == 'GetGuildInfo'){
+		req = JSON.parse(defenseListStart);
+		resp = JSON.parse(defenseList);
+		reqDeckLog = JSON.parse(deckLogStart);
+		respDeckLog = JSON.parse(deckLog);
+		}
+	  //Populate the Defense_Deck Table
+	  if (resp['command'] == 'GetGuildSiegeBaseDefenseUnitList' || resp['command']=='GetGuildSiegeBaseDefenseUnitListPreset' || resp['command']=='GetGuildSiegeDefenseDeckByWizardId') {
+      //If wizard id and rating doesn't exist in wizardBattles[] then push to it
+      try {
+        defenseInfo = {}
+		tempDefenseDeckInfo = [];
+		sendDecks = {}
+        defenseFound = false;
+
+		for (var deck in resp['defense_deck_list']){
+
+			
+			  defenseInfo = {}
+			  defenseInfo.wizard_id = resp['defense_deck_list'][deck].wizard_id;
+			  defenseInfo.deck_id = resp['defense_deck_list'][deck].deck_id;
+			  unitCount = 0;
+			  for (var defenseUnit in resp['defense_unit_list']) {2
+
+				  if (defenseInfo.deck_id == resp['defense_unit_list'][defenseUnit].deck_id && resp['defense_unit_list'][defenseUnit].pos_id == 1 && resp['defense_unit_list'][defenseUnit].hasOwnProperty('unit_info')) {
+					  
+				  defenseInfo.uniqueMon1 = resp['defense_unit_list'][defenseUnit].unit_info.unit_id;
+				  defenseInfo.mon1 = resp['defense_unit_list'][defenseUnit].unit_info.unit_master_id;
+				  unitCount ++;
+				  }
+				  if (defenseInfo.deck_id == resp['defense_unit_list'][defenseUnit].deck_id && resp['defense_unit_list'][defenseUnit].pos_id == 2 && resp['defense_unit_list'][defenseUnit].hasOwnProperty('unit_info')) {
+				  defenseInfo.uniqueMon2 = resp['defense_unit_list'][defenseUnit].unit_info.unit_id;
+				  defenseInfo.mon2 = resp['defense_unit_list'][defenseUnit].unit_info.unit_master_id;
+				  unitCount ++;
+				  }
+				  if (defenseInfo.deck_id == resp['defense_unit_list'][defenseUnit].deck_id && resp['defense_unit_list'][defenseUnit].pos_id ==3 && resp['defense_unit_list'][defenseUnit].hasOwnProperty('unit_info')) {
+				  defenseInfo.uniqueMon3 = resp['defense_unit_list'][defenseUnit].unit_info.unit_id;
+				  defenseInfo.mon3 = resp['defense_unit_list'][defenseUnit].unit_info.unit_master_id;
+				  unitCount ++;
+				  }
+				;
+			}
+			//sort mon2 and mon3
+			if (unitCount == 3) {
+				if(defenseInfo.mon3 < defenseInfo.mon2) {
+					tempMon = defenseInfo.uniqueMon2;
+					tempMon2 = defenseInfo.mon2;
+					defenseInfo.uniqueMon2 = defenseInfo.uniqueMon3;
+					defenseInfo.mon2 = defenseInfo.mon3;
+					defenseInfo.uniqueMon3 = tempMon;
+					defenseInfo.mon3 = tempMon2;
+					
+				}
+				defenseInfo.deckPrimaryKey = defenseInfo.wizard_id.toString() + "_" + defenseInfo.uniqueMon1.toString() + "_" + defenseInfo.uniqueMon2.toString() + "_" + defenseInfo.uniqueMon3.toString();
+					
+			  tempDefenseDeckInfo.push(defenseInfo)
+			}
+
+		}
+		sendDecks.command = "SWGTSiegeDeckUnits";
+		sendDecks.deck_units = tempDefenseDeckInfo;
+		sendResp = sendDecks;
+		this.writeToFile(proxy, req, sendResp,'SWGT2-');
+      } catch (e) {
+        proxy.log({ type: 'debug', source: 'plugin', name: this.pluginName, message: `${resp['command']}-${e.message}` });
+      }
+    }
+
+	//Populate the Defense_Deck Log Matching Table
+	
+	  if (resp['command'] == 'GetGuildSiegeBattleLogByDeckId') {
+      //If wizard id and rating doesn't exist in wizardBattles[] then push to it
+
+      try {
+		
+        targetdeckid = req['target_deck_id'];
+		sendDecks = {}
+		deckLogLink=[]
+		//find the deckid info that matches in the tempDefenseDeckInfo
+
+		for (var k = tempDefenseDeckInfo.length - 1; k >= 0; k--) {
+            if (tempDefenseDeckInfo[k].deck_id == req['target_deck_id']) {
+				deckIDPrimaryKey = tempDefenseDeckInfo[k].wizard_id.toString() + "_" + tempDefenseDeckInfo[k].uniqueMon1.toString() + "_" + tempDefenseDeckInfo[k].uniqueMon2.toString() + "_" + tempDefenseDeckInfo[k].uniqueMon3.toString();
+			}
+		}
+		for ( var siegewar in resp['log_list']){
+			for (var battleLog in resp['log_list'][siegewar].battle_log_list){
+				//add each battle to deckLogLink
+				deckLogValues = {}
+				deckLogValues.deckIDPrimaryKey = deckIDPrimaryKey;
+				deckLogValues.wizard_id = resp['log_list'][siegewar].battle_log_list[battleLog].wizard_id;
+				deckLogValues.wizard_name = resp['log_list'][siegewar].battle_log_list[battleLog].wizard_name;
+				deckLogValues.opp_wizard_id = resp['log_list'][siegewar].battle_log_list[battleLog].opp_wizard_id;
+				deckLogValues.opp_wizard_name = resp['log_list'][siegewar].battle_log_list[battleLog].opp_wizard_name;
+				deckLogValues.win_lose = resp['log_list'][siegewar].battle_log_list[battleLog].win_lose;
+				deckLogValues.log_type = resp['log_list'][siegewar].battle_log_list[battleLog].log_type;
+				deckLogValues.log_timestamp = resp['log_list'][siegewar].battle_log_list[battleLog].log_timestamp;
+				deckLogValues.linkPrimaryKey = deckLogValues.wizard_id + "_" + deckLogValues.opp_wizard_id + "_" + deckLogValues.log_timestamp
+				deckLogLink.push(deckLogValues)
+			}
+		}
+		sendDecks.command = "SWGTSiegeDeckHistoryLink";
+		sendDecks.deck_log_history = deckLogLink;
+		sendResp = sendDecks;
+		this.writeToFile(proxy, req, sendResp,'SWGT3-');
+		
+		
+      } catch (e) {
+        proxy.log({ type: 'debug', source: 'plugin', name: this.pluginName, message: `${resp['command']}-${e.message}` });
+      }
+    }
+  },
   hasCacheMatch(proxy, config, req, resp, cache) {
     if (!this.hasAPISettings(config)) return false;
 
@@ -365,7 +509,7 @@ module.exports = {
     };
     cache[action] = JSON.stringify(resp);
     return false;
-  },
+  },  
   uploadToWebService(proxy, config, req, resp,endpointType) {
     if (!this.hasAPISettings(config)) return;
     const { command } = req;
